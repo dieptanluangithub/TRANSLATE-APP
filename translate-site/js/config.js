7e085cf26e5e5edfd2fde91c99ff27d740f9c3de
Diệp Tấn Luân <dieptanluanwork@gmail.com>
2021-12-24 15:08:44 +0700

    1

--- translate-site/js/config.js
+++ translate-site/js/config.js
@@ -1,3 +1,3 @@
 const availibleLanguages =['es', 'fr', 'pt', 'de', 'ar', 'ru', 'ja', 'zh', 'it'];
-const awsRegion = 'US_EAST_2';
-const poolId = 'us-east-2:0c407363-d3ff-433c-930b-7a2fa2bc3592';
+const awsRegion = 'us-east-1';
+const poolId = 'AKIARXPVFERQOG7RA66J';
--- translate_all.py
+++ translate_all.py
@@ -1,12 +1,9 @@
-#!/usr/local/bin/python2.7
-# coding: utf-8
-
 import boto3
 import argparse
 
 # command line arguments
 parser = argparse.ArgumentParser(description='Translate text to all possible languages supported by Amazon Translate')
-parser.add_argument('text_to_translate', default='Hello, World', nargs='?', help='Text to translate (default=\"Hello, World\")')
+parser.add_argument('text_to_translate', default='Xin chào buổi sáng', nargs='?', help='Text to translate (default=\"Hello, World\")')
 args = parser.parse_args()
 #print(args)
 
@@ -19,6 +16,6 @@ for lang, flag in lang_flag_pairs:
     print(flag)
     print(translate.translate_text(
         Text=args.text_to_translate,
-        SourceLanguageCode="en",
+        SourceLanguageCode="vi",
         TargetLanguageCode=lang
     )['TranslatedText'])
